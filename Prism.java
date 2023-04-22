b48cf04d421f184b26a8c84bd65cdcb50a0bb99d
Jordanwhattt <114823165+Jordanwhattt@users.noreply.github.com>
2023-04-22 17:41:00 -0400

    Add files via upload

---
+++ Prism.java
@@ -0,0 +1,11 @@
+package prj3;
+
+public class Prism {
+    private int min; //Lower Left Corner
+    private int max;
+    
+    public Prism(int low, int high) {
+        this.min = low;
+        this.max = high;
+    }
+}
